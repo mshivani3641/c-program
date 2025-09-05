3a2fce3b871ad13188b834e9c7b0a81670c5efa3
mshivani3641 <mshivani3641@gmail.com>
2025-09-05 19:46:00 +0530

    Add files via upload

---
+++ geometric series in c.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+#include <math.h> 
+void main() 
+{ 
+float x,sum,t,d; 
+int i,n; 
+printf("Input the Value of x :"); 
+scanf("%f",&x); 
+printf("Input the number of terms : "); 
+scanf("%d",&n); 
+sum =1; 
+for (i=1;i<n;i++) 
+{ 
+t=pow(x,i); 
+sum =sum+ t; 
+} 
+printf("\nthe sum = %f\nNumber of terms = %d\nvalue of x = %f\n",sum,n,x);//gcc sqsum.c -lm 
+}
